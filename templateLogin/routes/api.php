331c841031ba9246764bd546d5b24cd0aea8e712
Vanessa <vanessa-almeida@prodesan.com.br>
2022-10-21 11:49:00 -0300

    atualizando rotas

--- templateLogin/routes/api.php
+++ templateLogin/routes/api.php
@@ -16,8 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/turnos', 'App\Http\Controllers\TurnoController@buscaTurnos')
     ->name('get.turnos');
+// Route::middleware('auth:api')->get('/turnos', 'App\Http\Controllers\TurnoController@buscaTurnos')
+//     ->name('get.turnos');
 
-Route::post('auth/login', 'App\Http\Controllers\AuthController@login');
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
